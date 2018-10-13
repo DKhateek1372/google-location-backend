37f979dcf9322438d57888e39bbef24bfe8c6d8f
deepak <didwaniya.deepak13@gmail.com>
2018-10-13 21:54:06 +0530

    moovlee backend assignment

---
+++ index.js
@@ -0,0 +1,42 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var morgan = require('morgan');
+var mongoose = require('mongoose');
+var jwt = require('jsonwebtoken');
+var config = require('./config');
+var User = require('./models/user.js');
+var location = require('./models/location.js');
+var port = process.env.PORT || 8080
+var routes = require('./routes/routes.js');
+var cors = require('cors');
+var mongoUrl = config.mongo.url + config.mongo.dbName;
+console.log("mongoUrl", mongoUrl);
+mongoose.connect(mongoUrl);
+app.set('superSecret', config.secret);
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(morgan('dev'));
+
+app.get('/', function(req, res) {
+  res.send('Hello! The API is at http://localhost:' + port + '/api');
+});
+app.get('/setup', function(req, res) {
+  // create a sample user
+  var nick = new User({
+    firstName: 'deepak',
+    lastName: 'khateek',
+    email: 'didwaniya.deepak13@gmail.com',
+    password: 'password'
+  });
+  // save the sample user
+  nick.save(function(err) {
+    if (err) throw err;
+    console.log('User saved successfully');
+    res.json({ success: true });
+  });
+});
+app.use('/api', routes);
+app.listen(port);
+console.log('port is going to listen at http://localhost:' + port);
